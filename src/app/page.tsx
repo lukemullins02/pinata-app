8e5a2fc6190679a49f84a4b19b21342b53de05f5
noahprev <118709062+noahprev@users.noreply.github.com>
2025-02-08 14:49:22 -0500

    Added blurred background to landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { PinataSDK } from "pinata-web3";
 
-const pinata = new PinataSDK({
-  pinataJwt: process.env.PINATA_JWT!,
-  pinataGateway: "brown-secure-emu-26.mypinata.cloud",
-});
-
-const js = "";
 
 export default function Home() {
   const [file, setFile] = useState<File | null>(null);
@@ -69,56 +62,60 @@ export default function Home() {
   };
 
   return (
-    <main className="w-full min-h-screen bg-emerald-600 flex flex-col justify-center items-center p-6 relative">
-      <h1 className="absolute mt-4 top-6 text-white text-4xl font-bold">
-        Welcome to Recipe Storage!
-      </h1>
-
-      <input
-        type="file"
-        className="px-6 py-2 bg-white text-black text-xl font-semibold rounded shadow-lg hover:bg-gray-100 transition-transform transform"
-        onChange={handleChange}
-        accept="application/pdf, image/png, image/jpeg" // Restrict file types here
-      />
-      {error && <p className="text-red-500 mt-2">{error}</p>}
-      <button
-        className="mt-4 px-6 py-2 bg-white text-black text-xl font-semibold rounded shadow-lg hover:bg-gray-100 transition-transform transform"
-        type="button"
-        disabled={uploading}
-        onClick={uploadFile}
-      >
-        {uploading ? "Uploading..." : "Upload"}
-      </button>
-      {url && (
-        <div className="mt-6">
-          <img
-            src={url}
-            alt="Uploaded"
-            className="w-64 h-64 object-cover rounded-lg border border-gray-300 shadow-md"
+    <main className="w-full min-h-screen bg-cover bg-center bg-[url(/recipeBook3.jpg)] flex flex-col justify-center items-center p-6 relative">
+      <div className="bg-black bg-opacity-65 rounded px-20 py-20">
+        <div className="flex flex-col justify-center items-center ">
+          <h1 className="absolute mt-4 top-6 text-white bg-black rounded bg-opacity-65 py-2 px-4 text-4xl font-bold">
+            Welcome to Recipe Storage!
+          </h1>
+          <input
+            type="file"
+            className="px-6 py-2 bg-white text-black text-xl font-semibold rounded shadow-lg hover:bg-gray-100 transition-transform transform"
+            onChange={handleChange}
+            accept="application/pdf, image/png, image/jpeg" // Restrict file types here
           />
-          <p>{url.substring(48)}</p>
+          {error && <p className="text-red-500 mt-2">{error}</p>}
+          <button
+            className="mt-4 px-6 py-2 bg-white text-black text-xl font-semibold rounded shadow-lg hover:bg-gray-100 transition-transform transform"
+            type="button"
+            disabled={uploading}
+            onClick={uploadFile}
+          >
+            {uploading ? "Uploading..." : "Upload"}
+          </button>
+          {url && (
+            <div className="mt-6">
+              <img
+                src={url}
+                alt="Uploaded"
+                className="w-64 h-64 object-cover rounded-lg border border-gray-300 shadow-md"
+              />
+              <p>{url.substring(48)}</p>
+            </div>
+          )}
+          </div>
+          <div className="flex flex-col items-center justify-center">
+          <input
+            id="userinput"
+            className="px-4 py-2 mt-4 text-black border rounded"
+          />
+          <button
+            className="mt-2 px-6 py-2 bg-white text-black text-xl font-semibold rounded shadow-lg hover:bg-gray-100 transition-transform transform"
+            onClick={userInput}
+          >
+            Return CID File
+          </button>
+        
+          {input && (
+            <a
+              href={`https://brown-secure-emu-26.mypinata.cloud/ipfs/${input}`}
+              target="_blank"
+            >
+              Link for CID: {input}
+            </a>
+          )}
         </div>
-      )}
-
-      <input
-        id="userinput"
-        className="px-4 py-2 mt-4 text-black border rounded"
-      />
-      <button
-        className="mt-2 px-6 py-2 bg-white text-black text-xl font-semibold rounded shadow-lg hover:bg-gray-100 transition-transform transform"
-        onClick={userInput}
-      >
-        Return CID File
-      </button>
-
-      {input && (
-        <a
-          href={`https://brown-secure-emu-26.mypinata.cloud/ipfs/${input}`}
-          target="_blank"
-        >
-          Link for CID: {input}
-        </a>
-      )}
+      </div>
     </main>
   );
 }
